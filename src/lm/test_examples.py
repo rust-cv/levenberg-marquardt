Fix norm calculation in tests

src/lm/test_examples.py
@@ -4,13 +4,57 @@ from scipy.optimize import leastsq
 np.set_printoptions(precision=20)
 
 
-def report(out):
+def enorm(v):
+    rdwarf = 3.834e-20
+    rgiant = 1.304e19
+    agiant = rgiant / v.size
+
+    s1 = s2 = s3 = x1max = x3max = 0.
+
+    for i in range(v.size):
+        xabs = abs(v[i])
+
+        if xabs > rdwarf and xabs < agiant:
+            s2 += xabs**2
+        elif xabs <= rdwarf:
+            if xabs <= x3max:
+                if xabs != 0.:
+                    s3 += (xabs / x3max)**2
+            else:
+                s3 = 1 + s3 * (x3max / xabs)**2
+                x3max = xabs
+        else:
+            if xabs <= x1max:
+                s1 += (xabs / x1max)**2
+            else:
+                s1 = 1. + s1 * (x1max / xabs)**2
+                x1max = xabs
+
+    if s1 != 0.:
+        return x1max * np.sqrt(s1 + (s2 / x1max) / x1max)
+
+    if s2 == 0.:
+        return x3max * np.sqrt(s3)
+
+    if s2 >= x3max:
+        return np.sqrt(s2 * (1 + (x3max / s2) * (x3max * s3)))
+
+    return np.sqrt(x3max * ((s2 / x3max) + (x3max * s3)))
+
+
+def report(f, out):
     print("params = \n  {}".format(out[0]))
-    print("objective_function = {}".format((out[2]['fvec'] ** 2).sum() * 0.5))
+    fvec = out[2]['fvec']
+    print("objective_function (fvec) = {}".format((enorm(fvec) ** 2) * 0.5))
+    # fvec2 = f(out[0])
+    # print("objective_function (fvec2) = {}".format((enorm(fvec2) ** 2) * 0.5))
     print("terminate {}, {}".format(out[3], out[4]))
 
 
 def linear_full_rank(n, m, factor=1.):
+    print("\n" + "=" * 80)
+    print("linear_full_rank {} {}".format(n, m))
+    print("=" * 80)
     def func(params):
         s = params.sum()
         temp = 2. * s / m + 1
@@ -30,6 +74,9 @@ def linear_full_rank(n, m, factor=1.):
 
 
 def linear_rank1(n, m, factor=1.):
+    print("\n" + "=" * 80)
+    print("linear_rank1 {} {}".format(n, m))
+    print("=" * 80)
     def func(params):
         vec = np.zeros(m)
         s = 0
@@ -50,6 +97,9 @@ def linear_rank1(n, m, factor=1.):
 
 
 def linear_rank1_zero(n, m, factor=1.):
+    print("\n" + "=" * 80)
+    print("linear_rank1_zero {} {}".format(n, m))
+    print("=" * 80)
     def func(params, vec=np.zeros(m)):
         s = 0
         for j in range(1, n - 1):
@@ -70,6 +120,9 @@ def linear_rank1_zero(n, m, factor=1.):
 
 
 def rosenbruck():
+    print("\n" + "=" * 80)
+    print("rosenbruck")
+    print("=" * 80)
     def func(params, vec=np.zeros(2)):
         vec[0] = 10 * (params[1] - params[0]**2)
         vec[1] = 1 - params[0]
@@ -86,6 +139,9 @@ def rosenbruck():
 
 
 def helical_valley():
+    print("\n" + "=" * 80)
+    print("helical_valley")
+    print("=" * 80)
     tpi = 2 * np.pi
 
     def func(params, vec=np.zeros(3)):
@@ -124,6 +180,9 @@ def helical_valley():
 
 
 def powell_singular():
+    print("\n" + "=" * 80)
+    print("powell_singular")
+    print("=" * 80)
     def func(params, vec=np.zeros(4)):
         vec[0] = params[0] + 10 * params[1]
         vec[1] = np.sqrt(5) * (params[2] - params[3])
@@ -150,6 +209,62 @@ def powell_singular():
     return func, jac, np.asfarray([3, -1, 0, 1])
 
 
+def freudenstein_roth():
+    print("\n" + "=" * 80)
+    print("freudenstein_roth")
+    print("=" * 80)
+
+    def func(params, vec=np.zeros(2)):
+        vec[0] = -13 + params[0] + ((5 - params[1]) * params[1] - 2) * params[1]
+        vec[1] = -29 + params[0] + ((1 + params[1]) * params[1] - 14) * params[1]
+        return vec
+
+    def jac(params, jac=np.zeros((2,2))):
+        jac[0] = 1
+        jac[1,0] = params[1] * (10 - 3 * params[1]) - 2
+        jac[1,1] = params[1] * (2 + 3 * params[1]) - 14
+        return jac.T
+
+    return func, jac, np.asfarray([0.5, -2])
+
+
 tol = 1.49012e-08
+
+f, jac, x0 = linear_full_rank(5, 10)
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+f, jac, x0 = linear_full_rank(5, 50)
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
 f, jac, x0 = linear_rank1(5, 10)
-report(leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+f, jac, x0 = linear_rank1(5, 50)
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
+f, jac, x0 = linear_rank1_zero(5, 10)
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+f, jac, x0 = linear_rank1_zero(5, 50)
+report(f, leastsq(f, x0, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
+f, jac, x0 = rosenbruck()
+for scale in (1., 10., 100.):
+    print("initial: {}".format(x0))
+    print("SCALE = {}".format(scale))
+    report(f, leastsq(f, x0 * scale, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
+f, jac, x0 = helical_valley()
+for scale in (1., 10., 100.):
+    print("initial: {}".format(x0))
+    print("SCALE = {}".format(scale))
+    report(f, leastsq(f, x0 * scale, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
+f, jac, x0 = powell_singular()
+for scale in (1., 10., 100.):
+    print("initial: {}".format(x0))
+    print("SCALE = {}".format(scale))
+    report(f, leastsq(f, x0 * scale, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))
+
+f, jac, x0 = freudenstein_roth()
+for scale in (1., 10., 100.):
+    print("initial: {}".format(x0))
+    print("SCALE = {}".format(scale))
+    report(f, leastsq(f, x0 * scale, Dfun=jac, ftol=tol, xtol=tol, gtol=0., full_output=True))